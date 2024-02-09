28a40a6a9e125248e8936452bb951f9575527b54
Tasneem <thalim.glam@gmail.com>
2024-02-09 15:19:44 -0800

    Updating webpack config js file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,28 +3,48 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      //this is webpack plugins to generate html and inject bundles
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+
+      // this plugins will custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+
+      // this will create manifest.json file
+
+      new WebpackPwaManifest({})
+
     ],
 
     module: {
       rules: [
-        
+
       ],
     },
   };
 };
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+
+// TODO: Add CSS loaders and babel to webpack.
