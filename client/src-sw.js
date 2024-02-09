62794cd6cf179fdcebeb80a946f896162c77e8bf
Tasneem <thalim.glam@gmail.com>
2024-02-09 12:01:14 -0800

    Setting up assets cache inside src-sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -5,7 +5,8 @@ const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
-precacheAndRoute(self.__WB_MANIFEST);
+precacheAndRoute(self.__WB_MANIFEST); // Here the precacheAndRoute() method takes an array of URLs to precache.
+// The self._WB_MANIDEST is an array that contains the list of URLs to precache.
 
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
@@ -23,8 +24,24 @@ warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
-
+//-------------------------------------------------------------------------------------------
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+//---------------------------------- Setting asset cache -----------------------------------------
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination), //defining the callback function that will filter the requests needed in cache
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache', // naming the asset cache storage
+    plugins: [
+      //setting up plugins to save cache response to a certain time limit
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
