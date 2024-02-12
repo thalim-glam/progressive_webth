ad436db0143543f3cd0d0272c0243307d9922268
Tasneem <thalim.glam@gmail.com>
2024-02-11 22:38:50 -0800

    Updating the database.js file by editing PUT and GET methods

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,39 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// TODO: Add logic to a method that accepts some content and adds it to the database------------------------------------
+export const putDb = async (content) => {
+  console.log('PUT to the database');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const contactDb = await openDB('jate', 1); // Creating connection to the database and version to use.
+
+  const tx = contactDb.transaction('jate', 'readwrite');   // Creating a new transaction and specify the database and data privilege
+
+  const store = tx.objectStore('jate'); //Openning the desired objedt store 
+
+  const request = store.put({ id: 1, value: content }); //Storing and passing content
+
+  const result = await request; // Confirmation of the request is assigned to result
+  console.log('Data is saved to the database', result);
+};
+
+// TODO: Add logic for a method that gets all the content from the database-------------------------------------------------
+export const getDb = async () => {
+
+  console.error('GET from the database');
+
+  const contactDb = await openDB('jate', 1); // Creating connection to the db and needed version
+
+  const tx = contactDb.transaction('jate', 'readonly'); // Creating new transaction and specify the database and data privileges
+
+  const store = tx.objectStore('jate');// Opennig the desired object store
+
+  const request = store.getAll(); // Using .getAll() methood to fetch all data
+
+  const result = await request; // Confirmation of the request is assigned
+
+  console.log('result.value', result);
+  return result?.value;
+};
 
 initdb();
